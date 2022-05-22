22082ffe7ba1b45a19a309bb25cfd395ac9bdaa0
christianlund <christian.lund0890@gmail.com>
2022-05-22 09:39:55 +0000

    Add event listener and runQuiz function

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+//Wait for the DOM to finish loading before running the game
+//Add event listners to mouse clicks
+
+document.addEventListener("DOMContentLoaded", function() {
+    button.addEventListener("click", runQuiz, {
+        once: true
+    })
+    runQuiz();
+})
+
+function runQuiz () {
+    quizQuestions.sort(() => Math.random() - 0.5);
+    nextQuestion();
+
+    questionElement.innerText = quizQuestions.question
+    optionA.innerText = quizQuestions.a
+    optionB.innerText = quizQuestions.b
+    optionC.innerText = quizQuestions.c
+    optionD.innerText = quizQuestions.d
+}
+
+function nextQuestion () {
+    
+}
+
+function checkAnswer () {
+    
+}
+
+function incrementScore () {
+    
+}
+
+function showResult () {
+    
+}
+
+function displayFinalScore () {
+    
+}
+
+const quizQuestions = [{
+    question: "When was the first Ford Model-T made?",
+    a: "1900",
+    b: "1910",
+    c: "1908",
+    d: "1912",
+    answer: "c",
+},
+{
+    question:
+}
+
+]
